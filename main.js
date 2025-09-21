22560235e77f93f12db55ebaa18188dc25c040e7
JaysonMatula <114369233+JaysonMatula@users.noreply.github.com>
2025-09-21 10:59:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-
+async function diceRoll () {
+try { 
+  const response = await new Promise((resolve) => {
+  const roll = Math.floor(Math.random() * 6) + 1;
+  resolve(roll);
+});
+  document.getElementById("die").value = response;
+  document.getElementById("roll").focus();
+} catch (e) {
+  console.log("There was a problem rolling the die.")
+  }
+}
